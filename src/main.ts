a3e53d471fb3c7ac82927cacea7c9fd4b35f7e7c
Val Ault <GTXC73@motorolasolutions.com>
2021-07-26 16:38:28 -0600

    fixed

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
-import MainConnection from './db-access/MainConnection';
 
 // Import custom theming
 // REPLACED by a line in vue.config.js
@@ -16,9 +15,6 @@ Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
 
 Vue.config.productionTip = false
-
-console.log("calling main...");
-new MainConnection().main();
 
 new Vue({
   router,
